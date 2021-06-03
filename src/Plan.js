185911a57594b5557248245719d937f260ab30ce
[suyash patel] <suyashpatelmrj@gmail.com>
2021-06-04 01:28:57 +0530

    initial commit

---
+++ src/Plan.js
@@ -0,0 +1,8 @@
+import './App.css';
+function Plan(props) {
+    return < >
+        <li  className="shadow p-2 my-2 col-sm-9"> { props.value } < /li>
+        <br />
+     < / >
+}
+export default Plan;
